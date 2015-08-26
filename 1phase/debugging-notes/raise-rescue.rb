ab262c6fc896b1327420735ccbe2ddff400d1415
BootCoder <bootcoder@gmail.com>
2015-08-25 23:33:25 -0700

    loop the r&r call

--- 1phase/debugging-notes/raise-rescue.rb
+++ 1phase/debugging-notes/raise-rescue.rb
@@ -1,16 +1,24 @@
 ############################
 ## Raise & Rescue Example ##
-### If you are into that ###
+############################
+###  If you're into you  ###
+## know... error handling ##
 ############################
 
 
+def raise_error
+  raise SyntaxError, '(ERROR) this error is a thing that is happenen'
+end
+
 
 def raise_and_rescue
+
+  print 'I can has nom nom :> '
+  $input = gets.chomp
+  puts 'I am before the raise.'
+
   begin
-    print 'I can has nom nom :> '
-    thingy = gets.chomp
-    puts 'I am before the raise.'
-    raise 'A super not awesome catastrofudge error has occurred.' unless thingy == ""
+    raise ArgumentError, '(ERROR) A super not awesome catastrofudge error has occurred.' unless $input == ""
       puts 'I am after the raise (if error).'
     rescue
       puts 'I am rescued. YAY!!!'
@@ -18,7 +26,9 @@ def raise_and_rescue
     else
       puts "I run if there is no error but I'm totes optional."
   end
+
   puts 'I am after the Raise-Rescue block.'
 end
 
-raise_and_rescue
+# raise_error
+raise_and_rescue until $input == "aa"
